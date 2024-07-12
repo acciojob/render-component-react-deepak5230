fff87b9a69110d6172ef2481faf1f7dea60bdee7
deepak <deepakgupta351@gmail.com>
2024-07-12 17:37:59 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+//dhbhfbdbhjdbhfbbdfj
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p>I am learning React.My life is getting better</p>
+    </div>
   )
 }
 
